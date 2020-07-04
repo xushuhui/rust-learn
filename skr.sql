abca77458fa753530da20cf7639b42d85827b8d0
xushuhui <474497097@qq.com>
2020-07-04 08:54:19 +0800

    project init

---
+++ skr.sql
@@ -0,0 +1,275 @@
+-- 账户模型
+CREATE TABLE `account_user` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '账号id',
+  `email` varchar(30) NOT NULL DEFAULT '' COMMENT '邮箱',
+  `phone` varchar(15) NOT NULL DEFAULT '' COMMENT '手机号',
+  `password` varchar(32) NOT NULL DEFAULT '' COMMENT '密码',
+  `create_at` int(11) NOT NULL DEFAULT '0' COMMENT '创建时间',
+  `create_ip_at` varchar(12) NOT NULL DEFAULT '' COMMENT '创建ip',
+  `last_login_at` int(11) NOT NULL DEFAULT '0' COMMENT '最后一次登录时间',
+  `last_login_ip_at` varchar(12) NOT NULL DEFAULT '' COMMENT '最后一次登录ip',
+  `login_times` int(11) NOT NULL DEFAULT '0' COMMENT '登录次数',
+  `status` tinyint(1) NOT NULL DEFAULT '0' COMMENT '状态 1:enable, 0:disable, -1:deleted',
+  PRIMARY KEY (`id`),
+  KEY `idx_email` (`email`),
+  KEY `idx_phone` (`phone`)
+
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='账户';
+
+-- 第三方账户
+CREATE TABLE `account_platform` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增id',
+  `uid` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '账号id',
+  `platform_id` varchar(60) NOT NULL DEFAULT '' COMMENT '平台id',
+  `platform_token` varchar(60) NOT NULL DEFAULT '' COMMENT '平台access_token',
+  `type` tinyint(1) NOT NULL DEFAULT '0' COMMENT '平台类型 0:未知,1:facebook,2:google,3:wechat,4:qq,5:weibo,6:twitter',
+  `nickname` varchar(60) NOT NULL DEFAULT '' COMMENT '昵称',
+  `avatar` varchar(255) NOT NULL DEFAULT '' COMMENT '头像',
+  `create_at` int(11) NOT NULL DEFAULT '0' COMMENT '创建时间',
+  `update_at` int(11) NOT NULL DEFAULT '0' COMMENT '更新时间',
+  PRIMARY KEY (`id`),
+  KEY `idx_uid` (`uid`),
+  KEY `idx_platform_id` (`platform_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='第三方用户信息';
+-- 用户模型
+CREATE TABLE `skr_member` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '用户id',
+  `uid` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '账号id',
+  `nickname` varchar(30) NOT NULL DEFAULT '' COMMENT '昵称',
+  `avatar` varchar(255) NOT NULL DEFAULT '' COMMENT '头像(相对路径)',
+  `gender` enum('male','female','unknow') NOT NULL DEFAULT 'unknow' COMMENT '性别',
+  `role` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '角色 0:普通用户 1:vip',
+  `create_at` int(11) NOT NULL DEFAULT '0' COMMENT '创建时间',
+  `update_at` int(11) NOT NULL DEFAULT '0' COMMENT '更新时间',
+  PRIMARY KEY (`id`),
+  KEY `idx_uid` (`uid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='账户信息';
+
+-- 员工表
+CREATE TABLE `staff_info` (
+    `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '员工id',
+    `uid` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '账号id',
+    `email` varchar(30) NOT NULL DEFAULT '' COMMENT '员工邮箱',
+    `phone` varchar(15) NOT NULL DEFAULT '' COMMENT '员工手机号',
+    `name` varchar(30) NOT NULL DEFAULT '' COMMENT '员工姓名',
+    `nickname` varchar(30) NOT NULL DEFAULT '' COMMENT '员工昵称',
+    `avatar` varchar(255) NOT NULL DEFAULT '' COMMENT '员工头像(相对路径)',
+    `gender` enum('male','female','unknow') NOT NULL DEFAULT 'unknow' COMMENT '员工性别',
+    `create_at` int(11) NOT NULL DEFAULT '0' COMMENT '创建时间',
+    `update_at` int(11) NOT NULL DEFAULT '0' COMMENT '更新时间',
+    PRIMARY KEY (`id`),
+    KEY `idx_uid` (`uid`),
+    KEY `idx_email` (`email`),
+    KEY `idx_phone` (`phone`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='员工信息(这里列了大概的信息，多的可以垂直拆表)';
+系统权限管理模型
+
+
+-- 权限管理: 系统map
+CREATE TABLE `auth_ms` (
+    `id` smallint(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增id',
+    `ms_name` varchar(255) NOT NULL DEFAULT '0' COMMENT '系统名称',
+    `ms_desc` varchar(255) NOT NULL DEFAULT '0' COMMENT '系描述',
+    `ms_domain` varchar(255) NOT NULL DEFAULT '0' COMMENT '系统域名',
+    `create_at` int(11) NOT NULL DEFAULT '0' COMMENT '创建时间',
+    `create_by` int(11) NOT NULL DEFAULT '0' COMMENT '创建人staff_id',
+    `update_at` int(11) NOT NULL DEFAULT '0' COMMENT '更新时间',
+    `update_by` int(11) NOT NULL DEFAULT '0' COMMENT '修改人staff_id',
+    `status` tinyint(1) NOT NULL DEFAULT '0' COMMENT '状态 1:enable, 0:disable, -1:deleted',
+    PRIMARY KEY (`id`),
+    KEY `idx_domain` (`ms_domain`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='系统map(登记目前存在的后台系统信息)';
+
+-- 权限管理: 系统menu
+CREATE TABLE `auth_ms_menu` (
+    `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增id',
+    `ms_id` smallint(11) unsigned NOT NULL DEFAULT '0' COMMENT '系统id',
+    `parent_id` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '父菜单id',
+    `menu_name` varchar(255) NOT NULL DEFAULT '0' COMMENT '菜单名称',
+    `menu_desc` varchar(255) NOT NULL DEFAULT '0' COMMENT '菜描述',
+    `menu_uri` varchar(255) NOT NULL DEFAULT '0' COMMENT '菜单uri',
+    `create_at` int(11) NOT NULL DEFAULT '0' COMMENT '创建时间',
+    `is_show` enum('yes','no') NOT NULL DEFAULT 'no' COMMENT '是否展示菜单',
+    `create_by` int(11) NOT NULL DEFAULT '0' COMMENT '创建人staff_id',
+    `update_at` int(11) NOT NULL DEFAULT '0' COMMENT '更新时间',
+    `update_by` int(11) NOT NULL DEFAULT '0' COMMENT '修改人staff_id',
+    `status` tinyint(1) NOT NULL DEFAULT '0' COMMENT '状态 1:enable, 0:disable, -1:deleted',
+    PRIMARY KEY (`id`),
+    KEY `idx_ms_id` (`ms_id`),
+    KEY `idx_parent_id` (`parent_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='系统menu';
+
+-- 权限管理: 系统权限
+CREATE TABLE `auth_item` (
+    `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增id',
+    `ms_id` tinyint(11) unsigned NOT NULL DEFAULT '0' COMMENT '系统id',
+    `menu_id` varchar(255) NOT NULL DEFAULT '0' COMMENT '页面/接口uri',
+    `create_at` int(11) NOT NULL DEFAULT '0' COMMENT '创建时间',
+    `create_by` int(11) NOT NULL DEFAULT '0' COMMENT '创建人staff_id',
+    `update_at` int(11) NOT NULL DEFAULT '0' COMMENT '更新时间',
+    `update_by` int(11) NOT NULL DEFAULT '0' COMMENT '修改人staff_id',
+    `status` tinyint(1) NOT NULL DEFAULT '0' COMMENT '状态 1:enable, 0:disable, -1:deleted',
+    PRIMARY KEY (`id`),
+    KEY `idx_ms_menu` (`ms_id`, `menu_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='系统权限';
+
+-- 权限管理: 系统权限(权限的各个集合)
+CREATE TABLE `auth_role` (
+    `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增id',
+    `name` varchar(255) NOT NULL DEFAULT '0' COMMENT '角色名称',
+    `desc` varchar(255) NOT NULL DEFAULT '0' COMMENT '角描述',
+    `auth_item_set` text COMMENT '权限集合 多个值,号隔开',
+    `create_at` int(11) NOT NULL DEFAULT '0' COMMENT '创建时间',
+    `create_by` int(11) NOT NULL DEFAULT '0' COMMENT '创建人staff_id',
+    `update_at` int(11) NOT NULL DEFAULT '0' COMMENT '更新时间',
+    `update_by` int(11) NOT NULL DEFAULT '0' COMMENT '修改人staff_id',
+    `status` tinyint(1) NOT NULL DEFAULT '0' COMMENT '状态 1:enable, 0:disable, -1:deleted',
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='员工角色';
+
+-- 权限管理: 角色与员工关系
+CREATE TABLE `auth_role_staff` (
+    `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增id',
+    `staff_id` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '员工id',
+    `role_set` text COMMENT '角色集合 多个值,号隔开',
+    `create_at` int(11) NOT NULL DEFAULT '0' COMMENT '创建时间',
+    `create_by` int(11) NOT NULL DEFAULT '0' COMMENT '创建人staff_id',
+    `update_at` int(11) NOT NULL DEFAULT '0' COMMENT '更新时间',
+    `update_by` int(11) NOT NULL DEFAULT '0' COMMENT '修改人staff_id',
+    `status` tinyint(1) NOT NULL DEFAULT '0' COMMENT '状态 1:enable, 0:disable, -1:deleted',
+    PRIMARY KEY (`id`),
+    KEY `idx_staff_id` (`staff_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='权限角色与员工关系';
+
+-- 品牌表 product_brands
+CREATE TABLE `product_brands` (
+    `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '品牌ID',
+    `name` varchar(255) unsigned NOT NULL DEFAULT '' COMMENT '品牌名称',
+    `desc` varchar(255) unsigned NOT NULL DEFAULT '' COMMENT '品牌描述',
+    `logo_url` varchar(255) unsigned NOT NULL DEFAULT '' COMMENT '品牌logo图片',
+    `create_at` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '创建时间',
+    `create_by` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '创建人staff_id',
+    `update_at` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '更新时间',
+    `update_by` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '修改人staff_id',
+    `status` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '状态 1:enable, 0:disable, -1:deleted',
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='品牌表';
+
+-- 类别表 product_category
+CREATE TABLE `product_category` (
+    `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '分类ID',
+    `pid` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '父ID',
+    `name` varchar(255) unsigned NOT NULL DEFAULT '' COMMENT '分类名称',
+    `desc` varchar(255) unsigned NOT NULL DEFAULT '' COMMENT '分类描述',
+    `pic_url` varchar(255) unsigned NOT NULL DEFAULT '' COMMENT '分类图片',
+    `path` varchar(255) unsigned NOT NULL DEFAULT '' COMMENT '分类地址{pid}-{child_id}-...',
+    `create_at` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '创建时间',
+    `create_by` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '创建人staff_id',
+    `update_at` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '更新时间',
+    `update_by` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '修改人staff_id',
+    `status` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '状态 1:enable, 0:disable, -1:deleted',
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='类别表';
+
+-- spu表 product_spu
+-- spu: standard product unit 标准产品单位
+CREATE TABLE `product_spu` (
+    `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT 'SPU ID',
+    `brand_id` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '品牌ID',
+    `category_id` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '分类ID',
+    `name` varchar(255) unsigned NOT NULL DEFAULT '' COMMENT 'spu名称',
+    `desc` varchar(255) unsigned NOT NULL DEFAULT '' COMMENT 'spu描述',
+    `selling_point` varchar(255) unsigned NOT NULL DEFAULT '' COMMENT '卖点',
+    `unit` varchar(255) unsigned NOT NULL DEFAULT '' COMMENT 'spu单位',
+    `banner_url` text COMMENT 'banner图片 多个图片逗号分隔',
+    `main_url` text COMMENT '商品介绍主图 多个图片逗号分隔',
+    `price_fee` int unsigned NOT NULL DEFAULT 0 COMMENT '售价，整数方式保存',
+    `price_scale` tinyint unsigned NOT NULL DEFAULT 0 COMMENT '售价，金额对应的小数位数',
+    `market_price_fee` int unsigned NOT NULL DEFAULT 0 COMMENT '市场价，整数方式保存',
+    `market_price_scale` tinyint unsigned NOT NULL DEFAULT 0 COMMENT '市场价，金额对应的小数位数',
+    `create_at` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '创建时间',
+    `create_by` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '创建人staff_id',
+    `update_at` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '更新时间',
+    `update_by` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '修改人staff_id',
+    `status` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '状态 1:enable, 0:disable, -1:deleted',
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT AUTO_INCREMENT=666666 CHARSET=utf8mb4 COMMENT='spu表';
+
+-- sku表 product_sku
+-- sku: stock keeping unit 库存量单位
+CREATE TABLE `product_sku` (
+    `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT 'SKU ID',
+    `spu_id` int(11) unsigned NOT NULL DEFAULT '0' COMMENT 'SPU ID',
+    `attrs` text COMMENT '销售属性值{attr_value_id}-{attr_value_id} 多个销售属性值逗号分隔',
+    `banner_url` text COMMENT 'banner图片 多个图片逗号分隔',
+    `main_url` text COMMENT '商品介绍主图 多个图片逗号分隔',
+    `price_fee` int unsigned NOT NULL DEFAULT 0 COMMENT '售价，整数方式保存',
+    `price_scale` tinyint unsigned NOT NULL DEFAULT 0 COMMENT '售价，金额对应的小数位数',
+    `market_price_fee` int unsigned NOT NULL DEFAULT 0 COMMENT '市场价，整数方式保存',
+    `market_price_scale` tinyint unsigned NOT NULL DEFAULT 0 COMMENT '市场价，金额对应的小数位数',
+    `create_at` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '创建时间',
+    `create_by` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '创建人staff_id',
+    `update_at` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '更新时间',
+    `update_by` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '修改人staff_id',
+    `status` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '状态 1:enable, 0:disable, -1:deleted',
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT AUTO_INCREMENT=666666 CHARSET=utf8mb4 COMMENT='sku表';
+
+-- 销售属性表 product_attr
+CREATE TABLE `product_attr` (
+    `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '销售属性ID',
+    `name` varchar(255) unsigned NOT NULL DEFAULT '' COMMENT '销售属性名称',
+    `desc` varchar(255) unsigned NOT NULL DEFAULT '' COMMENT '销售属性描述',
+    `create_at` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '创建时间',
+    `create_by` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '创建人staff_id',
+    `update_at` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '更新时间',
+    `update_by` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '修改人staff_id',
+    `status` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '状态 1:enable, 0:disable, -1:deleted',
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='销售属性表';
+
+-- 销售属性值 product_attr_value
+CREATE TABLE `product_attr_value` (
+    `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '销售属性值ID',
+    `attr_id` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '销售属性ID',
+    `value` varchar(255) unsigned NOT NULL DEFAULT '' COMMENT '销售属性值',
+    `desc` varchar(255) unsigned NOT NULL DEFAULT '' COMMENT '销售属性值描述',
+    `create_at` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '创建时间',
+    `create_by` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '创建人staff_id',
+    `update_at` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '更新时间',
+    `update_by` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '修改人staff_id',
+    `status` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '状态 1:enable, 0:disable, -1:deleted',
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='销售属性值';
+
+-- 关联关系冗余表 product_spu_sku_attr_map
+-- 1. spu下 有哪些sku
+-- 2. spu下 有那些销售属性
+-- 3. spu下 每个销售属性对应的销售属性值(一对多)
+-- 4. spu下 每个销售属性值对应的sku(一对多)
+CREATE TABLE `product_spu_sku_attr_map` (
+    `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增ID',
+    `spu_id` int(11) unsigned NOT NULL DEFAULT '0' COMMENT 'SPU ID',
+    `sku_id` int(11) unsigned NOT NULL DEFAULT '0' COMMENT 'SKU ID',
+    `attr_id` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '销售属性ID',
+    `attr_name` varchar(255) NOT NULL DEFAULT '0' COMMENT '销售属性名称',
+    `attr_value_id` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '销售属性值ID',
+    `attr_value_name` varchar(255) NOT NULL DEFAULT '0' COMMENT '销售属性值',
+    `create_at` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '创建时间',
+    `create_by` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '创建人staff_id',
+    `status` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '状态 1:enable, 0:disable, -1:deleted',
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='关联关系冗余表';
+
+-- sku库存表 product_sku_stock
+CREATE TABLE `product_sku_stock` (
+    `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增ID',
+    `sku_id` int(11) unsigned NOT NULL DEFAULT '0' COMMENT 'SKU ID',
+    `quantity` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '库存',
+    `create_at` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '创建时间',
+    `create_by` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '创建人staff_id',
+    `update_at` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '更新时间',
+    `update_by` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '修改人staff_id',
+    `status` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '状态 1:enable, 0:disable, -1:deleted',
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='sku库存表';
